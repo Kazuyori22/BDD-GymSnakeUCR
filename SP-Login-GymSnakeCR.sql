df260fe798157a3790469fbd683575de45b7cad5
Kenneth Oshima <128851006+Kazuyori22@users.noreply.github.com>
2025-11-08 03:51:47 -0600

    Update SP-Login-GymSnakeCR.sql

--- SP-Login-GymSnakeCR.sql
+++ SP-Login-GymSnakeCR.sql
@@ -10,3 +10,28 @@ BEGIN
     FROM Cliente
     WHERE Correo = @Correo;
 END
+
+-----------------------------------------------------------------------------------------
+
+CREATE PROCEDURE ValidarLoginCliente
+    @Correo NVARCHAR(100),
+    @Contrasenia NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        CedulaCliente,
+        Nombre,
+        PrimerApellido,
+        SegundoApellido,
+        FechaNacimiento,
+        Correo,
+        Telefono,
+        FechaRegistro,
+        RutaFotoCliente
+    FROM Cliente
+    WHERE Correo = @Correo
+      AND Contrasenia = @Contrasenia;
+END;
+GO
